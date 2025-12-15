6c0903d5695982ccb708bbc9295d8423595eb77d
jolypo <koolase783@gmail.com>
2025-12-15 21:02:58 +0300

    Update cloudinary.js

--- Controllers/cloudinary.js
+++ Controllers/cloudinary.js
@@ -1,10 +1,16 @@
-
-const cloudinary = require('cloudinary').v2;
+import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports = cloudinary;
+export const getSignedPDFUrl = (publicId) => {
+  return cloudinary.url(publicId, {
+    resource_type: "raw",        // PDF أو ملفات أخرى
+    type: "authenticated",       // الرابط محمي
+    expires_at: Math.floor(Date.now() / 1000) + 60 * 5 // صالح 5 دقائق
+  });
+};
+
