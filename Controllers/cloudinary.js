b7dbaff5fe72ce21ef87c1a6b86f864020e6d7dd
jolypo <koolase783@gmail.com>
2025-12-15 21:10:06 +0300

    Update cloudinary.js

--- Controllers/cloudinary.js
+++ Controllers/cloudinary.js
@@ -1,4 +1,5 @@
-import { v2 as cloudinary } from "cloudinary";
+// src/Controllers/cloudinary.js
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -6,4 +7,4 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinary;
+module.exports = cloudinary;
